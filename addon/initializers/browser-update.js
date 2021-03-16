2e41f047c2bde63587a8cec5816defd0b941df9a
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-16 12:07:16 +0100

    Add FastBoot support
    This prevents browserUpdate from running in FastBoot since it uses browser APIs.

--- addon/initializers/browser-update.js
+++ addon/initializers/browser-update.js
@@ -1,9 +1,11 @@
 import browserUpdate from 'browser-update';
 
 export function initialize(application) {
-  if (application.hasRegistration('config:environment')) {
-    const config = application.resolveRegistration('config:environment');
-    browserUpdate(config['browserUpdate']);
+  if(typeof FastBoot === 'undefined') {
+    if (application.hasRegistration('config:environment')) {
+      const config = application.resolveRegistration('config:environment');
+      browserUpdate(config['browserUpdate']);
+    }
   }
 }
 
